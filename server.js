1095e80ab1e974ffb1a82f2e14fa730a7ab20658
amimbs <andymimbs91@gmail.com>
2022-03-19 02:44:50 -0400

    Committing the completed project

---
+++ server.js
@@ -0,0 +1,30 @@
+// express setup
+const express = require('express');
+
+// app setup
+const app = express();
+const ceoList = require('./ceo_list');
+
+// set the template with exoress
+app.set('view engine', 'ejs');
+
+// requests
+app.get('/api', (req, res) => {
+    res.render('index');
+});
+
+app.get('/api/ceo-list', (req, res) => {
+    res.render('ceo_list', { ceo_list: ceoList });
+});
+
+app.get('/api/ceo-list/:slug', (req, res) => {
+    let foundCeo = ceoList.find((ceo) => ceo.slug == req.params.slug);
+    res.render('ceos_year', { displayCeo: foundCeo });
+});
+
+// server 
+app.listen(3000, () => {
+    console.log('App is now listening on port 3000 . . .')
+});
+
+
